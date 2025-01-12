cfe63fd210860f807bcfcfa84f6cf62dbf94c798
Bilal-Zaro <bilalza2004@gmail.com>
2025-01-12 18:00:00 +0300

    nmnm

---
+++ index.js
@@ -0,0 +1,63 @@
+// const arr=[2,3,4];
+/***********************************************for*************************************************************/
+
+// const newarr = [];
+// function way1(num){
+// for(let i =0 ;i<num.length;i++){
+//     newarr[i]=2**arr[i];
+// }
+// return newarr;
+// }
+// console.log(way1(arr));
+/************************************************foreach****************************************************************/
+// function way2(num){
+// num=2**num;
+// console.log(num);
+// }
+// arr.forEach(way2);
+
+/****************************************************map***************************************************************/
+// function way3(num){
+//     return 2**num;
+// }
+// const newarr =arr.map(way3);
+// console.log(newarr);
+/******************************************************Q2***************************************************************/
+// const arr = [1, 2, 3, "rawan"];
+// function jnjn(num) {
+//     if(num%2===0){
+//         return "even";
+//     }
+//     else if(num%2===1){
+//         return "odd";
+//     }
+//     else {
+//         return "N/A";
+//     }
+// }
+// const newarr =arr.map(jnjn);
+// console.log(newarr);
+/****************************************************Q3**********************************************************/
+// const names =["bilal","rania","yara","ruslan","bte5"];
+// function nmnm(num){
+// console.log(num);
+// }
+// const newnames=names.forEach(nmnm);
+/**********************************************ََQ4*********************************************************************/
+// function fizzbuzz(num){
+// if(num%3==0 && num%5==0){
+//     return "Fizz Buzz";
+// }
+// else if(num%3==0){
+// return "Fizz";
+// }
+// else if(num%5==0){
+//     return "Buzz";
+// }
+// else{
+//     return num;
+// }
+// }
+// const arr=[15,3,5,4];
+// const newarr=arr.map(fizzbuzz);
+// console.log(newarr);
